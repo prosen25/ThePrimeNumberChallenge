4ab70c9353aa777a83b8edfacae54b50f062cd2e
Prosenjit Mondal <contact.prosen25@gmail.com>
2024-12-09 20:31:06 +0000

    added logic for prime number check

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,35 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        int counter = 0;
+        int startNumber = 10;
+        int endNumber = 50;
 
+        System.out.println("Here are the first three prime number between " + startNumber + " and " + endNumber);
+
+        for (int i = startNumber; i <= endNumber; i++) {
+            if (isPrime(i)) {
+                System.out.println("Number " + i + " is a prime number.");
+                counter++;
+
+                if (counter == 3) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static boolean isPrime(int number) {
+
+        if (number <= 2) {
+            return (number == 2);
+        }
+
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
